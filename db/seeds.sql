b76ddd087d392ea299fbe1ca4430a4bac1f66274
Green S <kaylasierrabartley@gmail.com>
2021-03-14 19:50:16 -0500

    added more if statements to inquirer .then function

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,29 @@
-INSERT INTO department (id, name)
+INSERT INTO department (name)
 VALUES
-    (1, 'Front Line'),
-    (2, 'Tehnology'),
-    (3, 'Business');
+    ('Front Line'),
+    ('Tehnology'),
+    ('Business');
 
-INSERT INTO role (id, title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES 
-    (1, 'Cashier', 28157.17, 1),
-    (2, 'Financial Analyst', 52643.47, 3),
-    (3, 'Software Engineer', 83428.64, 2),
-    (4, 'Cyber Engineer', 94170.08, 2), 
-    (5, 'CEO', 146000.12, 3),
-    (6, 'Janitor', 21378.59, 1),
-    (7, 'Shift Manager', 41714.32, 1),
-    (8, 'VP of Operations', 135571.54, 3);
+    ('Cashier', 28157.17, 1),
+    ('Financial Analyst', 52643.47, 3),
+    ('Software Engineer', 83428.64, 2),
+    ('Cyber Engineer', 94170.08, 2), 
+    ('CEO', 146000.12, 3),
+    ('Janitor', 21378.59, 1),
+    ('Shift Manager', 41714.32, 1),
+    ('VP of Operations', 135571.54, 3);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
-    (1, 'Kayla', 'Bartley', 3, 5),
+    (1, 'Kayla', 'Bartley', 3, 2),
     (2, 'Chelsea', 'Newman', 5, NULL),
-    (3, 'Charmaine', 'Green', 1, 7),
-    (4, 'Damari', 'Clarke', 2, 8), 
-    (5, 'Monique', 'Clayton', 4, 5),
-    (6, 'Jillian', 'Romeo', 7, 8),
-    (7, 'Judy', 'Dale', 1, 7),
-    (8, 'Symone', 'Cole', 6, 7),
-    (9, 'Lucille', 'Fley', 8, 5);
+    (3, 'Charmaine', 'Green', 1, 6),
+    (4, 'Damari', 'Clarke', 2, 9), 
+    (5, 'Monique', 'Clayton', 4, 2),
+    (6, 'Jillian', 'Romeo', 7, 9),
+    (7, 'Judy', 'Dale', 1, 6),
+    (8, 'Symone', 'Cole', 6, 6),
+    (9, 'Lucille', 'Fley', 8, 2);
 
--- index.js
+++ index.js
@@ -25,6 +25,131 @@ const connection = mysql.createConnection({
     password: 'Jamaica',
     database: 'tracker'
 });
+function addRole(){
+
+}
+function addDepartment(a){
+    console.log(a);
+        connection.promise().query('INSERT INTO department (name) VALUES (?)', [a] )
+        .catch(console.log);
+}
+async function options(action){
+    if(action === 'Exit'){
+        return 0;
+    }
+    if(action === 'View All Departments'){
+        connection.promise().query("SELECT * FROM department")
+        .then( ([rows,fields]) => {
+            console.log('\n\n');
+            console.table(rows);
+            console.log('\n\n');
+        })
+        .catch(console.log)
+        .then(()=>{return prompt()});
+        
+    }
+    if(action === 'View All Roles'){
+        connection.promise().query("SELECT role.*, department.name AS 'Department Name' FROM role LEFT JOIN department ON role.department_id = department.id ")
+        .then( ([rows,fields]) => {
+            console.log('\n\n');
+            console.table(rows);
+            console.log('\n\n');
+        })
+        .catch(console.log)
+        .then(()=>{return prompt()});
+    }
+    if(action === 'View All Employees'){
+        connection.promise().query("SELECT e.id, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS 'Job Title', role.salary, department.name AS 'Department Name', CONCAT(m.first_name , ' ' ,m.last_name) AS 'Manager' FROM employee e LEFT JOIN role ON e.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee m ON e.manager_id = m.id")
+        .then( ([rows,fields]) => {
+            console.log('\n\n');
+            console.table(rows);
+            console.log('\n\n');
+        })
+        .catch(console.log)
+        .then(()=>{return prompt()});
+    }
+    if(action === 'Add Department'){
+        inquirer
+            .prompt(
+                {
+                    type: 'input',
+                    name: 'department',
+                    message: 'Enter department name: ',
+                    validate: input => {
+                        if (input) {
+                        return true;
+                        } else {
+                        console.log('Please enter a department name!');
+                        return false;
+                        }
+                    }
+                }
+            ).then(({department})=>{
+                connection.promise().query('INSERT INTO department (name) VALUES (?)', [department] )
+                .catch(console.log);
+            })
+            .then(()=>{return prompt()});
+        
+    }
+    if(action === 'Add Role'){
+        let roleArray =[];
+        connection.promise()
+            .query("SELECT * FROM role")
+            .then( ([rows,fields]) => {
+                rows.forEach(item => {
+                    roleArray.push(item.title);
+                })
+                console.log(roleArray);   
+            })
+        await inquirer
+            .prompt(
+                [
+                    {
+                        type: 'input',
+                        name: 'title',
+                        message: 'Enter job title: ',
+                        validate: input => {
+                            if (input) {
+                            return true;
+                            } else {
+                            console.log('Please enter a job title!');
+                            return false;
+                            }
+                        }
+                    },
+                    {
+                        type: 'input',
+                        name: 'salary',
+                        message: 'Enter salary: ',
+                        validate: input => {
+                            if (input) {
+                            return true;
+                            } else {
+                            console.log('Please enter a salary!');
+                            return false;
+                            }
+                        }
+                    },
+                    {
+                        type: 'list',
+                        pageSize: 20,
+                        message: 'Select role',
+                        name: 'role',
+                        choices: roleArray
+                    }
+                ]
+            ).then(({role})=>{
+                connection.promise().query("SELECT * FROM department WHERE id LIKE %?% ", [role])
+                .then( ([rows,fields]) => {
+                    console.table(rows);
+                });
+               // .query("INSERT INTO role (title, salary, department_id) VALUES ('Cashier', 28157.17, 1),, [department] )
+               // .catch(console.log);
+            })
+            .then(()=>{return prompt()});
+        
+    }
+}
 
 function prompt(){
     inquirer
@@ -33,43 +158,90 @@ function prompt(){
                 type: 'list',
                 pageSize: 20,
                 message: 'What would you like to do?',
-                name: 'action',
-                choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'All Employee', 'Update Employee Role', 'Exit']
+                name: 'choice',
+                choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Exit']
               }
         )
-        .then(({action}) => {
-            if(action === 'Exit'){
-                return 0;
-            }
-            if(action === 'View All Departments'){
-                connection.promise().query("SELECT * FROM employee")
+        .then(({choice}) => {
+                options(choice);
+                
+        });
+          /*  if(action === 'Add Employee'){
+                connection.promise()
+                .query("SELECT * FROM role")
                 .then( ([rows,fields]) => {
-                    console.log('\n\n');
-                    console.table(rows);
-                    console.log('\n\n');
+                    let roleArray =[];
+                    rows.forEach(item => {
+                        roleArray.push(item.title);
+                    })
+                    console.log(roleArray);   
                 })
-                .catch(console.log);
-            }
-            if(action === 'View All Roles'){
-                connection.promise().query("SELECT role.*, department.name FROM role LEFT JOIN department ON role.department_id = department.id ")
-                .then( ([rows,fields]) => {
-                    console.log('\n\n');
-                    console.table(rows);
-                    console.log('\n\n');
+
+                inquirer
+                        .prompt(
+                            {
+                                type: 'input',
+                                name: 'firstName',
+                                message: 'Enter first name: ',
+                                validate: input => {
+                                    if (input) {
+                                    return true;
+                                    } else {
+                                    console.log('Please enter a first name!');
+                                    return false;
+                                    }
+                                }
+                            },
+                            {
+                                type: 'input',
+                                name: 'lastName',
+                                message: 'Enter last name: ',
+                                validate: input => {
+                                    if (input) {
+                                    return true;
+                                    } else {
+                                    console.log('Please enter a last name!');
+                                    return false;
+                                    }
+                                }
+                            },
+                            {
+                                type: 'list',
+                                pageSize: 20,
+                                message: 'Select Role',
+                                name: 'role',
+                                choices: roleArray
+                            }
+                        );
+                connection.promise()
+                .query("SELECT COUNT(*) FROM department")
+                .then([rows,fields]) => {
+                    rows.forEach(item => {
+                        const count = item.COUNT(*);
+                    })
+                    let manageridArray = [null];
+                    var i;
+                    for(i=1, i<count+1, i++){
+                        manageridArray.push(i);
+                    }
+
+                    inquirer
+                        .prompt(
+                            {
+                                type: 'list',
+                                pageSize: 20,
+                                message: 'Select Manager ID',
+                                name: 'manager',
+                                choices: manageridArray
+                            }
+                        )
                 })
-                .catch(console.log);
-            }
-            if(action === 'View All Employees'){
-                connection.promise().query("SELECT m.first_name AS 'Manager', e.* , role.title, role.salary, department.name FROM employee e LEFT JOIN role ON e.role_id = role.id LEFT JOIN department ON ?? department.id LEFT JOIN employee m ON e.manager_id = m.id ")
-                .then( ([rows,fields]) => {
-                    console.log('\n\n');
-                    console.table(rows);
-                    console.log('\n\n');
-                })
-                .catch(console.log);
-            }
-        })
-        .then(()=> {prompt();});
+                .then(console.log(roleArray));
+            
+               
+            
+    
+            }*/
         
 }
 
